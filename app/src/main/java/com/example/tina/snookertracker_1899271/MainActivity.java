e454540ee80a499207f43d6d5df6109a43462d6d
TinaLuan <tinaluan9794@gmail.com>
2018-02-06 21:16:19 +0000

    toggle and foul done; can update team score(need a bit modification later though);

--- app/src/main/java/com/example/tina/snookertracker_1899271/MainActivity.java
+++ app/src/main/java/com/example/tina/snookertracker_1899271/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     public static final String NAME3 = "name3";
     public static final String NAME4 = "name4";
 
+    public static final int FOUL_PENALTY = 4;
+
     private int activeScoreId;
 
     @Override
@@ -22,7 +24,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Intent intent = getIntent();
-        String name1 = intent.getStringExtra(NAME1);
+        String name1 = intent.getStringExtra(NAME1);System.out.println("M|"+name1+"|");
         String name2 = intent.getStringExtra(NAME2);
         String name3 = intent.getStringExtra(NAME3);
         String name4 = intent.getStringExtra(NAME4);
@@ -32,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         ((TextView)findViewById(R.id.nameDisplay4)).setText(name4);
 
         //System.out.println(name1 + "  " + name2 + name3 + name4);
-        activeScoreId = R.id.score3;
+        activeScoreId = R.id.score1;
 
     }
 
@@ -50,14 +52,86 @@ public class MainActivity extends AppCompatActivity {
         ((TextView)findViewById(scoreId)).setText(Integer.toString(score));
     }
 
+    private void updatePlayerScore(int scoreId, int increment) {
+        System.out.println(" id: "+ scoreId + " score: " + increment);
+
+        ((TextView)findViewById(scoreId)).setText( Integer.toString(getPlayerScore(activeScoreId) + increment) );
+
+        updateTeamScore(scoreId, increment);
+    }
+
     private int getPlayerScore(int scoreId) {
         return Integer.parseInt( ((TextView)findViewById(scoreId)).getText().toString() );
     }
-    public void onBalls(View view) {
+
+    private int getTeamScore(int teamScoreId) {
+        return Integer.parseInt( ((TextView)findViewById(teamScoreId)).getText().toString() );
+    }
+
+    private void updateTeamScore(int playerScoreId, int increment) {
+        int teamScoreId = R.id.teamScore1;;
+        switch(playerScoreId) {
+            case R.id.score1:
+            case R.id.score2:
+                teamScoreId = R.id.teamScore1;
+                break;
+            case R.id.score3:
+            case R.id.score4:
+                teamScoreId = R.id.teamScore2;
+                break;
+            default:
+                break;
+        }
+
+        ((TextView)findViewById(teamScoreId)).setText( Integer.toString(getTeamScore(teamScoreId) + increment) );
+    }
+
+    private void switchActivePlayer() {
+        switch (activeScoreId) {
+            case R.id.score1:
+                activeScoreId = R.id.score3;
+                System.out.println("switch from 1" + " to 3");
+                break;
+            case R.id.score2:
+                activeScoreId = R.id.score4;
+                System.out.println("switch from 2" + " to 4");
+                break;
+            case R.id.score3:
+                activeScoreId = R.id.score2;
+                System.out.println("switch from 3" + " to 2");
+                break;
+            case R.id.score4:
+                activeScoreId = R.id.score1;
+                System.out.println("switch from 4" + " to 1");
+                break;
+            default:
+                break;
+        }
+
+    }
+
+    public void onBall(View view) {
+        System.out.println("Ball----");
         int ballPoints = Integer.parseInt( ((Button)view).getText().toString() );
 
-        setPlayerScore(activeScoreId, ballPoints + getPlayerScore(activeScoreId));
+        updatePlayerScore(activeScoreId, ballPoints);
 
+    }
+
+    public void onToggle(View view) {
+        System.out.println("toggle-----");
+        switchActivePlayer();
+    }
+
+    public void onFoul(View view) {
+        System.out.println("FOUL-----");
+        switchActivePlayer();
+        updatePlayerScore(activeScoreId, FOUL_PENALTY);
+
+    }
+
+    public void onEnd(View view) {
+        System.out.println("END-----");
     }
 
 }
--- app/src/main/java/com/example/tina/snookertracker_1899271/SetupActivity.java
+++ app/src/main/java/com/example/tina/snookertracker_1899271/SetupActivity.java
@@ -8,6 +8,11 @@ import android.widget.EditText;
 
 public class SetupActivity extends AppCompatActivity {
 
+    public static final String DEFAUL_PLAYER_NAME_1 = "whatever1";
+    public static final String DEFAUL_PLAYER_NAME_2 = "whatever2";
+    public static final String DEFAUL_PLAYER_NAME_3 = "whatever3";
+    public static final String DEFAUL_PLAYER_NAME_4 = "whatever4";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +26,18 @@ public class SetupActivity extends AppCompatActivity {
         String name4 = ((EditText)findViewById(R.id.userName4)).getText().toString();
         //System.out.println(name1 + "  " + name2);
 
+        if (name1.length() == 0) {name1 = DEFAUL_PLAYER_NAME_1;}
+        if (name2.length() == 0) {name2 = DEFAUL_PLAYER_NAME_2;}
+        if (name3.length() == 0) {name3 = DEFAUL_PLAYER_NAME_3;}
+        if (name4.length() == 0) {name4 = DEFAUL_PLAYER_NAME_4;}
+
         Intent intent = new Intent(this, MainActivity.class);
+
         intent.putExtra(MainActivity.NAME1, name1);
         intent.putExtra(MainActivity.NAME2, name2);
         intent.putExtra(MainActivity.NAME3, name3);
         intent.putExtra(MainActivity.NAME4, name4);
+
         startActivity(intent);
     }
 }
